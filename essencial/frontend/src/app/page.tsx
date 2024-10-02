6561ca8e2c97dd8a6b7403545822c8664e39568e
GeisilaCosta <geisilascosta@gmail.com>
2024-10-02 13:55:47 -0300

    construindo os cards e o front da aplicacao

--- essencial/frontend/src/app/page.tsx
+++ essencial/frontend/src/app/page.tsx
@@ -1,5 +1,12 @@
-export default function Home() {
-  return (
-   <div>Aplicação</div>
-  );
+import ProdutoItem from '@/components/produto/ProdutoItem'
+import { produtos } from '@/core'
+
+export default function Inicio() {
+    return (
+        <div className="grid grid-cols-4 gap-5 container">
+            {produtos.map((produto) => (
+                <ProdutoItem key={produto.id} produto={produto} />
+            ))}
+        </div>
+    )
 }
